57cb8932b20f4e901feab8c5b0319f7fafd1fec9
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-01-10 10:17:36 +0100

    Updates

---
+++ precache-manifest.0b77a1ad47f9cbd58f2e3bc5700dff88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58018db131eb0ccd9da2b51b3de8fad1",
+    "url": "/MarkDown/index.html"
+  },
+  {
+    "revision": "56d96372f0ed2ccd4f57",
+    "url": "/MarkDown/static/css/main.19779289.chunk.css"
+  },
+  {
+    "revision": "09dfbf995173e575195d",
+    "url": "/MarkDown/static/js/2.41752b64.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MarkDown/static/js/2.41752b64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56d96372f0ed2ccd4f57",
+    "url": "/MarkDown/static/js/main.e8a8b4b4.chunk.js"
+  },
+  {
+    "revision": "c0f03dbab65484e1aeba",
+    "url": "/MarkDown/static/js/runtime-main.cfb0d688.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MarkDown/precache-manifest.0b77a1ad47f9cbd58f2e3bc5700dff88.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MarkDown/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
